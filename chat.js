67084d93889a7922f16b0659673a8b41ae5c3f63
xolugna <xolugna@gmail.com>
2019-08-07 17:47:25 +0200

    slow mode added, using cookie by default need to change

--- chat.js
+++ chat.js
@@ -1,6 +1,6 @@
 function chat(http) {
 
-    const redis_words = require('./redis');
+    const redis = require('./redis');
     const {ip} = require('./config');
 
 
@@ -24,33 +24,44 @@ const colors = [
 
     // socket io config
     const io = require('socket.io')(http);
-   // const redis = require('socket.io-redis');
-   // io.adapter(redis({ host: ip, port: 6379 }));
+    const socket_redis = require('socket.io-redis');
+    io.adapter(socket_redis({ host: ip, port: 6379 }));
 
     io.on("connection", (socket) => {
 
     // join room
-    socket.on("joinRoom", () => {
-        // ensure just connected to one database
-        socket.leaveAll();
+    socket.on("joinRoom", async () => {
         // assign random name and save it to redis
         // assign radnom color
         const room = socket.request.headers.referer.split("/").slice(-1)[0];
+        await redis.getRedisVariable(room) ? redis.increment(room) : redis.setRedisVariable(room, 1); 
         socket.join(room);
         socket.to(room).emit("success", `You have succesfully join the ${room} room`);        
     });
 
     // send and recieve message
-    socket.on("send", (msg) => {
-        // slow mode? every use just can chat every 30 secs
+    socket.on("send", async(msg) => {
+        // slow mode? every use just can chat every X secs
+        let cookie = socket.request.headers.cookie.substring(3);
         const room = socket.request.headers.referer.split("/").slice(-1)[0];
-        socket.nsp.to(room).emit("recieve", msg);
+        let wrote_30sec = await redis.getRedisVariable(cookie);
+        if (wrote_30sec === room) {
+            let response = "Slow mode activated, can write every X secs"
+            io.of('/').sockets[socket.id].emit("recieve",response);
+        }else {
+            redis.setRedisVariable(cookie, room, 18);
+            socket.nsp.to(room).emit("recieve", msg);
+        }
+        
     });
     
     // disconnect
-    socket.on('disconnect', function(){
-        // delete it from database
-        console.log('user disconnected');
+    socket.on('disconnect', async () => {
+        // delete it
+        const room = socket.request.headers.referer.split("/").slice(-1)[0];
+        redis.decrement(room);
+        let connections = await redis.getRedisVariable(room);
+        console.log(`user from ${room} disconnected, ${connections} connections`);
     });
 
     })  
--- redis.js
+++ redis.js
@@ -15,13 +15,13 @@ const client = redis.createClient({
 const getAsync = promisify(client.get).bind(client);
 const lrangeAsync = promisify(client.lrange).bind(client);
 
-function setRedisVariable(key, val,noExpire){
-    if(noExpire)client.set(key, JSON.stringify(val));
-    else client.set(key, JSON.stringify(val), 'EX', 300 );
+function setRedisVariable(key, val,expire){
+    if(!expire)client.set(key, JSON.stringify(val));
+    else client.set(key, JSON.stringify(val), 'EX', expire );
 }
 
 function getRedisVariable(key){
-    return client.getAsync(key).then(function(res) {
+    return getAsync(key).then(function(res) {
         return JSON.parse(res);
     }).catch(function(err){
         console.log("REDIS FAILED:"+err.message);
@@ -33,7 +33,7 @@ function pushRedisVariable(key, val){
 }
 
 function getRedisList(key){
-    return client.lrange(key, 0, -1).then(function(res) {
+    return lrangeAsync(key, 0, -1).then(function(res) {
         return res;
     }).catch(function(err){
         console.log(err.message);
@@ -49,6 +49,15 @@ function clearList(key){
     client.del(key);
 }
 
+function increment(key){
+    client.incr(key);
+}
+
+//operation is limited to 64 bit signed integers
+function decrement(key){
+    client.decr(key);
+}
+
 
 
 exports.setRedisVariable = setRedisVariable;
@@ -57,3 +66,5 @@ exports.pushRedisVariable = pushRedisVariable;
 exports.popRedisVariable = popRedisVariable;
 exports.getRedisList = getRedisList;
 exports.clearList = clearList;
+exports.increment = increment;
+exports.decrement = decrement;
